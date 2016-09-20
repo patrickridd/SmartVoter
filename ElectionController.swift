0cb1a8188f6636c61f5da5c7af935ae7103ce46d
pattyhackins <patrickridd@yahoo.com>
2016-09-20 12:06:40 -0600

    Setup ElectionController and ready to get back NSData

--- ElectionController.swift
+++ ElectionController.swift
@@ -10,6 +10,35 @@ import Foundation
 
 class ElectionController {
     
+    var electionDate = NSDate()
+    static let sharedController = ElectionController()
+    static let apiKey = "AIzaSyCJoqWI3cD5VRDcWzThID1ATEweZ5R7j9I"
+    static let baseURL = NSURL(string: "https://www.googleapis.com/civicinfo/v2/voterinfo")
+    
+    static func getContest(address: String, completion: ([Election]?) -> Void) {
+        
+        guard let url = baseURL else {
+            completion(nil)
+            return
+        }
+        
+        let urlParameters = ["address":address,"key":apiKey]
+        
+        NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters, body: nil) { (data, error) in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            print(data)
+            
+            
+        }
+        
+        
+    }
+    
+    
     
     
     
