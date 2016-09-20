9e7b5b29fd517f58acef7d49e0b7ec0b257763fb
pattyhackins <patrickridd@yahoo.com>
2016-09-20 12:52:41 -0600

    Placed all dictionaries in place to get specific data. Need to make sure my Url is correct because i'm not getting the Json back.

--- ElectionController.swift
+++ ElectionController.swift
@@ -10,10 +10,12 @@ import Foundation
 
 class ElectionController {
     
-    var electionDate = NSDate()
     static let sharedController = ElectionController()
     static let apiKey = "AIzaSyCJoqWI3cD5VRDcWzThID1ATEweZ5R7j9I"
     static let baseURL = NSURL(string: "https://www.googleapis.com/civicinfo/v2/voterinfo")
+    static var electionDate = String()
+    static var electionName = String()
+
     
     static func getContest(address: String, completion: ([Election]?) -> Void) {
         
@@ -25,13 +27,22 @@ class ElectionController {
         let urlParameters = ["address":address,"key":apiKey]
         
         NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters, body: nil) { (data, error) in
-            guard let data = data else {
-                completion(nil)
-                return
+            guard let data = data,
+                jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [String:AnyObject] else {
+//                electionDictionary = jsonDictionary["election"] as? [String:String],
+//                date = electionDictionary["electionDay"],
+//                electionName = electionDictionary["name"],
+//                contestsDictionary = jsonDictionary["contests"] as? [String:AnyObject] else {
+                    completion(nil)
+                    return
             }
             
-            print(data)
-            
+//            self.electionName = electionName
+//            self.electionDate = date
+//            print(electionName)
+//            print(electionDate)
+//            print(data)
+        print(jsonDictionary)
             
         }
         
