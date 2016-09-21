0351b1f980161261a51cb23143691c3ff28ecf31
Brad Forsyth <4sythb@gmail.com>
2016-09-21 10:35:52 -0600

    Fixed election model

--- Election.swift
+++ Election.swift
@@ -21,13 +21,13 @@ class Election {
     init?(dictionary: [String: AnyObject]) {
         guard let type = dictionary["type"] as? String,
             districtDictionary = dictionary["district"] as? [String : AnyObject],
-            scope = districtDictionary["scope"] as? String,
-            candidatesArray = dictionary["candidates"] as? [[String : AnyObject]] else { return nil }
+            scope = districtDictionary["scope"] as? String else { return nil }
         
         if type == "General" {
             let office = dictionary["office"] as? String
             
             self.office = office
+            
         } else if type == "Referendum" {
             let referendumTitle = dictionary["referendumTitle"] as? String
             let referendumSubtitle = dictionary["referendumSubtitle"] as? String
@@ -40,18 +40,20 @@ class Election {
         
         var candidates: [Candidate] = []
         
-        for candidateDictionary in candidatesArray {
-            guard let name = candidateDictionary["name"] as? String else {
-                return nil
+        if let candidatesArray = dictionary["candidates"] as? [[String : AnyObject]] {
+            for candidateDictionary in candidatesArray {
+                guard let name = candidateDictionary["name"] as? String else {
+                    return nil
+                }
+                
+                let party = candidateDictionary["party"] as? String
+                let webAddress = candidateDictionary["candidateUrl"] as? String
+                let phoneNumber = candidateDictionary["phone"] as? String
+                let emailAddress = candidateDictionary["email"] as? String
+                
+                let candidate = Candidate(name: name, party: party, websiteURL: webAddress, phone: phoneNumber, email: emailAddress)
+                candidates.append(candidate)
             }
-            
-            let party = candidateDictionary["party"] as? String
-            let  webAddress = candidateDictionary["candidateUrl"] as? String
-            let phoneNumber = candidateDictionary["phone"] as? String
-            let emailAddress = candidateDictionary["email"] as? String
-            
-            let candidate = Candidate(name: name, party: party, websiteURL: webAddress, phone: phoneNumber, email: emailAddress)
-            candidates.append(candidate)
         }
         
         self.type = type
