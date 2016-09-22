af1f7df44c3637bb6fc3946bec48abc3cd2aab5b
tsasselli <tsasselli@gmail.com>
2016-09-22 15:49:45 -0600

    setup MapView Annotation

--- SmartVoter/OfficialAddressMapViewController.swift
+++ SmartVoter/OfficialAddressMapViewController.swift
@@ -9,16 +9,64 @@
 import UIKit
 import MapKit
 
-class OfficialAddressMapViewController: UIViewController {
+class OfficialAddressMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    var address: String? 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var address: String?
+    var locationManager: CLLocationManager = CLLocationManager()
+    let regionRadius: CLLocationDistance = 3000
+    var currertLocatoin: CLLocation?
+    var coordinate: CLLocationCoordinate2D?
+    var geocoder: CLGeocoder = CLGeocoder()
+    var regionSet: Bool = false
+    var region: MKCoordinateRegion?
+    
+    var official: Official?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        setupMapView()
+        
     }
-
+    
+    func setupMapView () {
+        
+        self.locationManager.delegate = self
+        self.locationManager.distanceFilter = kCLHeadingFilterNone
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView?.showsUserLocation = true
+        self.mapView.delegate = self
+        self.mapView.scrollEnabled = true
+        guard let newAddress = address else { return }
+        forwardGeocodeAddress(newAddress) { (location) in
+            let annotation = MKPointAnnotation()
+            guard let coordinate = location?.coordinate else { return }
+            annotation.coordinate = coordinate
+            annotation.title = newAddress
+            
+            
+            
+        }
+    }
     
     
+    func forwardGeocodeAddress (address: String, completion: (location: CLLocation?) -> Void) {
+        let geoCoder = CLGeocoder()
+        var location = CLLocation?()
+        
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            if let placemark = placemarks?.first {
+                location = placemark.location
+            } else {
+                print("\(error!.localizedDescription)")
+            }
+            completion(location: location)
+        }
+        
+    }
 }
