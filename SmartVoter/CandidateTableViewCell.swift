d2200cc82d41cb3a4ac4dc90e58ef47fcf1800fc
pattyhackins <patrickridd@yahoo.com>
2016-09-22 17:59:27 -0600

    Updated ElectionDetail TableViewCell with information and color.

---
+++ SmartVoter/CandidateTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  CandidateTableViewCell.swift
+//  SmartVoter
+//
+//  Created by Patrick Ridd on 9/22/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import UIKit
+
+class CandidateTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateWith(candidate: Candidate) {
+        
+        nameLabel.text = candidate.name
+        partyLabel.text = candidate.party
+        phoneLabel.text = candidate.phone
+        emailLabel.text = candidate.email
+        websiteLabel.text = candidate.websiteURL
+        
+    }
+
+}
--- SmartVoter/ElectionDetailViewController.swift
+++ SmartVoter/ElectionDetailViewController.swift
@@ -14,9 +14,12 @@ class ElectionDetailViewController: UIViewController, UITableViewDelegate, UITab
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var dateLabel: UILabel!
     
     var contest: Contest?
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +28,8 @@ class ElectionDetailViewController: UIViewController, UITableViewDelegate, UITab
     
     func setupView() {
         guard let contest = contest else { return }
-        
+
+        dateLabel.text = "Election Day: \(contest.electionDay)"
         if contest.type == "General" {
             electionTypeLabel.text = contest.office
             descriptionLabel.text = contest.scope.capitalizedString
@@ -41,24 +45,32 @@ class ElectionDetailViewController: UIViewController, UITableViewDelegate, UITab
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWithIdentifier("candidateCell"), contest = contest, candidates = contest.candidates else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("candidateCell") as? CandidateTableViewCell, contest = contest, candidates = contest.candidates else { return UITableViewCell() }
 
         let candidate = candidates[indexPath.row]
-        
-        cell.textLabel?.text = candidate.name
-        cell.detailTextLabel?.text = candidate.party
+        cell.updateWith(candidate)
         
         
         
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Candidates"
     }
-    */
+    
+    func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let headerView = view as? UITableViewHeaderFooterView else {
+            return
+        }
+        
+        headerView.textLabel?.textColor = UIColor.whiteColor()
+        headerView.textLabel?.font = UIFont(name: "Avenir", size: 20.0)
+        headerView.contentView.backgroundColor = UIColor.redColor()
+        headerView.textLabel?.textAlignment = .Center
+    }
+  
+    
 }
 
 
