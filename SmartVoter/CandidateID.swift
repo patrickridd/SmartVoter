5380a16792b599b43e8493a2dbf81abc9cdf1889
tsasselli <tsasselli@gmail.com>
2016-09-29 14:58:10 -0600

    added network model

--- SmartVoter/CandidateID.swift
+++ SmartVoter/CandidateID.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+
+class CandidateID {
+    
+    private let kFullName = "fistlast"
+    private let kID = "cid"
+    private let kResponse = "response"
+    private let kAttributes = "@attributes"
+    private let kLegislator = "legislator"
+    
+    var fullName: String?
+    var candidateId: String?
+    
+    
+    init?(dictionary: [String: AnyObject])  {
+        
+        guard let response = dictionary[kResponse] as? [String : AnyObject],
+            let legislatorsArray = response[kLegislator] as? [[String : AnyObject]]     else { return nil }
+        
+        for legislator in legislatorsArray {
+            
+            guard let attributesDictionary = legislator[kAttributes] as? [String : AnyObject],
+                let cid = attributesDictionary[kID] as? String,
+                let fullName = attributesDictionary[kFullName] as? String else { return nil}
+            self.candidateId = cid
+            self.fullName = fullName
+        }
+    }
+}
--- SmartVoter/Contributions.swift
+++ SmartVoter/Contributions.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class Contributions {
+    
+//    private let kResponse = "response"
+//    private let kContributor = "contributor"
+//    private let kContributors = "contributors"
+//    private let kAttributes = "@attributes"
+//    private let kFullName = "cand_name"
+//    private let kOrganization = "org_name"
+//    private let kTotoal = "total"
+//    
+//   
+//    var organization: String?
+//    var total: String?
+//    
+//    init?(dictionary: [String: AnyObject])  {
+//        
+//        guard let response = dictionary[kResponse] as? [String : AnyObject],
+//        let contributors = response[kContributor] as? else { return nil }
+//        
+    
+}
