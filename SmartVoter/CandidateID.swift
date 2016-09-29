d1abc1e4d6c90eabaa42ad3159d51acd8dcab553
tsasselli <tsasselli@gmail.com>
2016-09-29 14:57:57 -0600

    added network models

---
+++ SmartVoter/CandidateID.swift
@@ -0,0 +1,9 @@
+//
+//  CandidateID.swift
+//  SmartVoter
+//
+//  Created by Travis Sasselli on 9/29/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import Foundation
---
+++ SmartVoter/Contributions.swift
@@ -0,0 +1,9 @@
+//
+//  Contributions.swift
+//  SmartVoter
+//
+//  Created by Travis Sasselli on 9/29/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import Foundation
