59174fb6e04fa5f19707527cee81a6160a68ce00
tsasselli <tsasselli@gmail.com>
2016-09-29 15:14:11 -0600

    updated models

--- SmartVoter/Contributions.swift
+++ SmartVoter/Contributions.swift
@@ -10,22 +10,31 @@ import Foundation
 
 class Contributions {
     
-//    private let kResponse = "response"
-//    private let kContributor = "contributor"
-//    private let kContributors = "contributors"
-//    private let kAttributes = "@attributes"
-//    private let kFullName = "cand_name"
-//    private let kOrganization = "org_name"
-//    private let kTotoal = "total"
-//    
-//   
-//    var organization: String?
-//    var total: String?
-//    
-//    init?(dictionary: [String: AnyObject])  {
-//        
-//        guard let response = dictionary[kResponse] as? [String : AnyObject],
-//        let contributors = response[kContributor] as? else { return nil }
-//        
+    private let kResponse = "response"
+    private let kContributor = "contributor"
+    private let kContributors = "contributors"
+    private let kAttributes = "@attributes"
+    private let kFullName = "cand_name"
+    private let kOrganization = "org_name"
+    private let kTotoal = "total"
+    
+   
+    var organization: String?
+    var total: String?
+    
+    init?(dictionary: [String: AnyObject])  {
+        
+        guard let response = dictionary[kResponse] as? [String : AnyObject],
+        let contributors = response[kContributors] as? [String : AnyObject],
+            let contributorArray = contributors[kContributor] as? [[String : AnyObject]] else { return nil }
+        
+        for contributor in contributorArray {
+            guard let attributesDictionary = contributor[kAttributes] as? [String : AnyObject],
+            let organization = attributesDictionary[kOrganization] as? String,
+                let total = attributesDictionary[kTotoal] as? String else { return nil }
+            self.organization = organization
+            self.total = total
+        }
+    }
     
 }
