df088eeb3edd393568ceac2d4932579f90b0aa07
tsasselli <tsasselli@gmail.com>
2016-09-30 11:05:46 -0600

    added barChart

---
+++ SmartVoter/BarChartViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BarChartViewController.swift
+//  SmartVoter
+//
+//  Created by Travis Sasselli on 9/30/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
