2715b6abda3304034e6eb2b323dbe54165d5928b
pattyhackins <patrickridd@yahoo.com>
2016-10-06 12:29:17 -0600

    Setup PageViewController

---
+++ SmartVoter/PageViewController.swift
@@ -0,0 +1,97 @@
+//
+//  PageViewController.swift
+//  SmartVoter
+//
+//  Created by Patrick Ridd on 10/6/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.instantiateViewController("SignUpViewController")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
+
+    }
+    
+    private func instantiateViewController(storyBoardID: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier(storyBoardID)
+    }
+    
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            firstViewControllerIndex = orderedViewControllers.indexOf(firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
+    
+    
+    
+}
+
+// MARK: UIPageViewControllerDataSource
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
