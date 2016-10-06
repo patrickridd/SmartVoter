4e48b14245f82837255b5fe2e0fc537f6aab95ba
tsasselli <tsasselli@gmail.com>
2016-10-06 10:40:04 -0600

    got charts working with mock

--- SmartVoter/BarChartViewController.swift
+++ SmartVoter/BarChartViewController.swift
@@ -15,6 +15,7 @@ class BarChartViewController: UIViewController {
     
     var official: Official?
     var candidates: [CandidateID]?
+    var months: [String]!
     
     
     override func viewDidLoad() {
@@ -27,27 +28,46 @@ class BarChartViewController: UIViewController {
             self.candidates = candidateIDs
             
         }
-     checkOfficialName()
+        checkOfficialName()
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(months, values: unitsSold)
+        
+        
     }
     
+    
+    func checkOfficialName() {
+        guard let officialName = official?.name,
+            let candidates = self.candidates else {
+                return
+        }
         
-        func checkOfficialName() {
-            guard let officialName = official?.name,
-                let candidates = self.candidates else {
-                    return
-            }
-            
-            let matchingCandidates = candidates.filter{$0.fullName == officialName}
-            if let candidate = matchingCandidates.first,
-                let id = candidate.candidateId
-            {
-                ContributorController.sharedController.fetchContributors(id, completion: { (contributoins) in
-                    print(contributoins)
-                })
-                
-            }
+        let matchingCandidates = candidates.filter{$0.fullName == officialName}
+        if let candidate = matchingCandidates.first,
+            let id = candidate.candidateId
+        {
+            ContributorController.sharedController.fetchContributors(id, completion: { (contributoins) in
+                print(contributoins)
+            })
             
         }
         
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [BarChartDataEntry] = []
         
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Money Spent")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+    }
 }
